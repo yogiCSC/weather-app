ebcf9d3c1273528c63c6aa720d42d6b8b86e8d6c
yogiCSC <kyogeswari.paddu2005@gmail.com>
2026-02-20 20:47:31 +0530

    Update script.js
    add script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function getWeather() {
     const city = document.getElementById("cityInput").value;
+
     document.getElementById("result").innerHTML =
         "Weather data for " + city + " will appear here.";
 }
+
